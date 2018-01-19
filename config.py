332f0e7fad918a6ce66bb54de91f428373863886
Kabus <kabusiblis@gmail.com>
2018-01-19 15:44:20 +0000

    Full capability for gameinfo.txt configuration.

--- config.py
+++ config.py
@@ -1,26 +1,59 @@
 import os, urllib.request
+
+# empty strings for initial run
+# todo save base directory and modified directory so that it can be reset without download.
 user_steam_directory = ""
+user_meta_directory = ""
 
 
 def config_build():
-    old_dir = "__INSTALL_DRIVE__"
-    new_dir = user_steam_directory
+    old_steam_dir = "__INSTALL_DRIVE__"
+    new_steam_dir = user_steam_directory
+    old_meta_dir = "__META_FILE_DRIVE__"
+    new_meta_dir = user_meta_directory
+
     filename = "game_compile/gameinfo.txt"
+
     # use with to check if dir is not original
     with open(filename) as file:
         build = file.read()
-        if old_dir not in build:
-            print('"{old_dir}" not found in {filename}.'.format(**locals()))
+        if old_steam_dir not in build:
+            print('"{old_steam_dir}" not found in {filename}.'.format(**locals()))
+            check = input("Woukd you like to reset your config to base settings? Y/N")
+            if check is "y":
+                print("Resetting path.")
+                reset_config()
+            elif check is "n":
+                pass
+            else:
+                print("Input unknown.")
+                return
             return
-
+        if old_meta_dir not in build:
+            print('"{old_meta_dir}" not found in {filename}.'.format(**locals()))
+            check = input("Woukd you like to reset your config to base settings? Y/N")
+            if check is "y":
+                print("Resetting path.")
+                reset_config()
+            elif check is "n":
+                pass
+            else:
+                print("Input unknown.")
+                return
+            return
     # Safely write the changed content, if found in the file
     with open(filename, 'w') as file:
-        print('Changing "{old_dir}" -> "{new_dir}" in {filename}'.format(**locals()))
-        build = build.replace(old_dir, new_dir)
+        print('Changing "{old_steam_dir}" -> "{new_steam_dir}" in {filename}'.format(**locals()))
+        build = build.replace(old_steam_dir, new_steam_dir)
+        print('Changing "{old_meta_dir}" -> "{new_meta_dir}" in {filename}'.format(**locals()))
+        build = build.replace(old_meta_dir, new_meta_dir)
         file.write(build)
+
 
 def reset_config():
     dl = urllib.request.URLopener()
+    print("Downloading gameinfo.text")
     dl.retrieve("https://raw.githubusercontent.com/KabusIblis/map-toolkit/master/game_compile/gameinfo.txt",
                 "game_compile/gameinfo.txt") # shoddy but should work
+    print("Finished reset on config gameinfo.text")
     pass
--- run.py
+++ run.py
@@ -9,7 +9,9 @@ def error():
 
 while True:
     try:
-        user_input = int(input("press 1 to compile(exits script) \n2 to change config\n3 to reset config text\n"))
+        user_input = int(input("Press 1 to compile (exits script) \n"
+                               "Press 2 to change config\n"
+                               "Press 3 to reset config text\n"))
     except ValueError:
         error()
         user_input = 0
@@ -18,13 +20,14 @@ while True:
         if user_input == 2:
             print("Only input hierarchy for directory before steamapps, eg Z:/Steam/steamapps only put Z:/Steam/")
             config.user_steam_directory = input("Enter your base steamapps directory: ")
+            config.user_meta_directory = input("Enter your metastruct directory if applicable: ")
             config.config_build()
         if user_input == 3:
-            confirm = input("are you sure you want to reset your gameinfo files? y/n")
+            confirm = input("Are you sure you want to reset your gameinfo files? y/n")
             if confirm is "y":
                 config.reset_config()
             elif confirm is "n":
-                print("returning to main")
+                print("Returning,")
             else:
                 error()
     finally:
